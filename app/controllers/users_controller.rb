bd23394541ca109ab7eb9cdabae4ff2ec71ab66d
navpreetsingh <navpreet.singh.k@gmail.com>
2013-10-07 12:55:26 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,32 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
-  def create
-    @user = User.new(params[:user])    # Not the final implementation!
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
-  end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-  def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  end
-    
+     
   def create
     @user = User.new(user_params)
     if @user.save
@@ -45,9 +20,9 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
+    def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
     
 end
